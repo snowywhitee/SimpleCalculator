14bd7119f56d1c727d9aec653f58ca5ae14d03a0
ALEXXXANDRO <alkolegov2011@yandex.ru>
2021-03-14 23:05:06 +0300

    Added Mod

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -73,8 +73,11 @@ namespace Calculator
         /// <returns></returns>
         public static double Div(double x, double y)
         {
-            if (y == 0.0) throw new DivideByZeroException();
             return x / y;
+        }
+        public static double Mod(double x, double y)
+        {
+            return x % y;
         }
     }
 }
